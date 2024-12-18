e4e52826c2076a22eff6e287125bff57442e7d89
hasanlq69 <hasanlq69@gmail.com>
2024-12-18 07:22:25 +0700

    Aplikasi Movie Database by Muhammad Hasan

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+
+  return (
+    <footer id="main-footer" className="text-white py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col text-center">
+            <p className="lead mb-0">Movie Database</p>
+            <p>All data provided by themoviedb.org API, All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
